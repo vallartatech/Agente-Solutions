f2dcdfa38d6af926790080dbfcaecdb56b4400c8
PedroPechKoh <ppechkoh@gmail.com>
2026-09-21 12:29:03 -0600

    feat: add main.jsx entry point for React application

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,4 @@
-//Esto es un cambio//import { StrictMode } from 'react'
+import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
